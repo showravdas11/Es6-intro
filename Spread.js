219b2fd1426167f219d18c14f9f5846b9831ded3
showravdas11 <showravdas11@gmail.com>
2021-08-23 18:08:08 +0600

    update

---
+++ Spread.js
@@ -0,0 +1,9 @@
+const numbers = [52, 65, 45, 82, 23, 34, 100]
+// console.log(numbers);
+// console.log(...numbers);
+
+// const max = Math.max(52, 65, 45, 82, 23, 34);
+// console.log(max);
+
+const maxArray = Math.max(...numbers);
+console.log(maxArray);
